30e262c4552f2b6fa51d84a49280af4d8350ac6a
prash1976 <prashtech344@gmail.com>
2019-01-29 11:34:58 +0530

    Initial Commit for the project

---
+++ InformAPI/src/main/java/com/prash/inform/InformApiApplication.java
@@ -0,0 +1,14 @@
+package com.prash.inform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformApiApplication.class, args);
+	}
+
+}
+
